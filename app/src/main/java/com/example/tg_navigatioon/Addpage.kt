0540a003bc611e3dccf6bd1c354b98274d255e20
Abdulloh_komilov <gitabdulloh@gmail.com>
2023-11-02 16:33:16 +0500

    just

---
+++ app/src/main/java/com/example/tg_navigatioon/Addpage.kt
@@ -0,0 +1,8 @@
+package com.example.tg_navigatioon
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AddPage(navController: NavController){
+}
